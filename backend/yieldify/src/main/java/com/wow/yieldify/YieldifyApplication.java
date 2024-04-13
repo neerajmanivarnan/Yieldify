83d7dad9d54082b26d62c89c7eb1ed31f3ca0428
Neeraj Manivarnan <neerajmanivarnan666@gmail.com>
2024-04-13 11:20:46 +0530

    project setup

---
+++ backend/yieldify/src/main/java/com/wow/yieldify/YieldifyApplication.java
@@ -0,0 +1,13 @@
+package com.wow.yieldify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YieldifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YieldifyApplication.class, args);
+	}
+
+}
---
+++ backend/yieldify/src/test/java/com/wow/yieldify/YieldifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wow.yieldify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YieldifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
